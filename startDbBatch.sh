6bd74a922955a428aae14ce4843d9751a99a7f0b
sunkjlee <kj00713@gmail.com>
2020-12-24 19:42:39 +0900

    clean

--- startDbBatch.sh
+++
@@ -1,9 +0,0 @@
-CLASS_PATH=.
-CLASS_PATH=$CLASS_PATH:/root/Gnew/dbBatch/lib/mybatis-3.5.3.jar
-CLASS_PATH=$CLASS_PATH:/root/Gnew/dbBatch/lib/postgresql-42.2.18.jar
-CLASS_PATH=$CLASS_PATH:/root/Gnew/dbBatch/bin
-export CLASS_PATH
-echo $CLASS_PATH
-#java -Xms1024m -Xmx1024m -DBatchMain -cp $CLASS_PATH com.gnew.bat.controller.BatchMain BatchMain /root/Gnew/dbBatch/conf/conf.properties
-
-java -DBatchMain -cp $CLASS_PATH com.gnew.bat.controller.BatchMain BatchMain /root/Gnew/dbBatch/conf/conf.properties
--- startDecryptoFile.sh
+++
@@ -1,8 +0,0 @@
-CLASS_PATH=.
-CLASS_PATH=$CLASS_PATH:/root/Gnew/decryptor/lib/postgresql-42.2.18.jar
-CLASS_PATH=$CLASS_PATH:/root/Gnew/decryptor/bin
-CLASS_PATH=$CLASS_PATH:/root/Gnew/decryptor/lib/decpBat.jar
-export CLASS_PATH
-echo $CLASS_PATH
-
-java -DDecryptFileBatch -cp $CLASS_PATH com.gnew.bat.main.DecryptFileBatch DecryptFileBatch /root/Gnew/decryptor/conf/conf.properties
